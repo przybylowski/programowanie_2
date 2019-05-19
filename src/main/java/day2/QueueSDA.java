0b9aaf9f7cef872afef26339c2ede5f85e25b838
Gilbert Przybylowski <gilbert.przybylowski@gmail.com>
2019-05-19 11:17:05 +0200

    v4

--- src/main/java/day2/QueueSDA.java
+++ src/main/java/day2/QueueSDA.java
@@ -1,23 +1,69 @@
 package day2;
 
-import java.util.Queue;
 
 public class QueueSDA {
     int capacity;
-
+    int head;
+    int tail;
+    int actualSize;
+    String tab[];
     public QueueSDA(int capacity) {
         this.capacity = capacity;
-        String tab [] = new String[capacity];
-
+        tab = new String[capacity];
+        head = 0;
+        actualSize = 0;
+        tail = 0;
     }
 
-    public int getSize() {
+
+
+    public int getCapacity() {
         return capacity;
     }
 
-    public boolean add() {
-
+    public boolean isFull() {
+        if (getActualSize() == capacity) {
+            return true;
+        }
         return false;
     }
+    public boolean isEmpty() {
+        return actualSize == 0;
+    }
+
+    public int getActualSize() {
+        return actualSize;
+    }
+
+    public void setActualSize(int actualSize) {
+        this.actualSize = actualSize;
+    }
+
+    public void add(String string) {
+
+        if (isFull() == false) {
+
+            tab[head] = string;
+            head++;
+            actualSize++;
+
+        }
+        else {System.out.println("Queue is Full");}
+
+    }
+
+    public String pop() {
+
+        if (isEmpty() == true)
+        {throw new NoItemsInQueue(); }
+
+        String returnedObject =  tab[tail];
+        this.tail++;
+        this.actualSize--;
+        return returnedObject;
+
+
+    }
+
 
 }
--- src/test/java/day2/QueueSDATest.java
+++ src/test/java/day2/QueueSDATest.java
@@ -7,13 +7,18 @@ public class QueueSDATest {
     @Test
     public void isCapacityGood() {
         QueueSDA queue = new QueueSDA(10);
-        Assert.assertEquals(queue.getSize(), 10);
+        Assert.assertEquals(queue.getCapacity(), 10);
     }
 
     @Test
     public void isStringAdded() {
         QueueSDA queue = new QueueSDA(10);
-        queue.addString("");
+        queue.add("Programowanie 1");
+        queue.add("Programowanie 2");
+        Assert.assertEquals(queue.getActualSize(),2);
+
+        Assert.assertEquals(queue.pop(),"Programowanie 1");
+        Assert.assertEquals(queue.pop(),"Programowanie 2");
     }
 
 }
